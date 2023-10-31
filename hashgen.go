865751e913651d571ff55e52a4be77eb3acf72bf
cyclone <34043806+cyclone-github@users.noreply.github.com>
2023-10-31 17:17:13 -0500

    2023-09-28.1730-hashplain
    fixed typo

--- hashgen.go
+++ hashgen.go
@@ -30,21 +30,23 @@ import (
 	"unicode/utf16"
 )
 
-// version history
-// v2022-12-15.2030; initial release
-// v2022-12-16.1800; fixed ntlm hash function, tweaked -w flag to be less restrictive, clean up code
-// v2022-12-17.2100; fixed typo in wordlist tag, added '-m plaintext' output mode (prints -w wordlist file to stdout)
-// v2022-12-20.1200; cleaned up bcrypt code
-// v2022-12-20.1430-goroutine; complete rewrite using goroutines & read/write buffers
-// v2022-12-21.1400-goroutine; added multiple new algo's including hashcat mode equivalents
-// v2022-12-23.1200-goroutine; added argon2id (very slow), added sync / wait group for future use, change h/s readout from millions to thousands,
-// v2022-12-24.1800-optimize; optimized all hashing functions, tweaked buffer size
-// v2023-03-15.0900-optimize; added "stdout", fixed "lines/sec" to show "M lines/sec", tweaked output buffer for stdout, tweaked sha2xxx flags to allow "shaxxx", ex: "sha512"
-// v2023-03-28.1155-optimize; added "stdin"
-// v2023-05-13.0000-optimize; optimized code all hashing functions for better performance
-// v2023-08-15.1900-hashplain; added: -hashplain flag for hash:plain output, support for $HEX[] wordlist, -cost flag for bcrypt, tweaked: write buffers & custom buffers for argon & bcrypt, tweaked logging outputs
-// v2023-08-16.1200-hashplain; added error correction to 'fix' improperly formatted $HEX[] lines
-// v2023-09-28.1730-hashplain; modify -hashplain flag to be encoding-agnostic
+/*
+version history
+v2022-12-15.2030; initial release
+v2022-12-16.1800; fixed ntlm hash function, tweaked -w flag to be less restrictive, clean up code
+v2022-12-17.2100; fixed typo in wordlist tag, added '-m plaintext' output mode (prints -w wordlist file to stdout)
+v2022-12-20.1200; cleaned up bcrypt code
+v2022-12-20.1430-goroutine; complete rewrite using goroutines & read/write buffers
+v2022-12-21.1400-goroutine; added multiple new algo's including hashcat mode equivalents
+v2022-12-23.1200-goroutine; added argon2id (very slow), added sync / wait group for future use, change h/s readout from millions to thousands,
+v2022-12-24.1800-optimize; optimized all hashing functions, tweaked buffer size
+v2023-03-15.0900-optimize; added "stdout", fixed "lines/sec" to show "M lines/sec", tweaked output buffer for stdout, tweaked sha2xxx flags to allow "shaxxx", ex: "sha512"
+v2023-03-28.1155-optimize; added "stdin"
+v2023-05-13.0000-optimize; optimized code all hashing functions for better performance
+v2023-08-15.1900-hashplain; added: -hashplain flag for hash:plain output, support for $HEX[] wordlist, -cost flag for bcrypt, tweaked: write buffers & custom buffers for argon & bcrypt, tweaked logging outputs
+v2023-08-16.1200-hashplain; added error correction to 'fix' improperly formatted $HEX[] lines
+v2023-09-28.1730-hashplain; modify -hashplain flag to be encoding-agnostic
+*/
 
 func versionFunc() {
 	fmt.Fprintln(os.Stderr, "Cyclone hash generator v2023-09-28.1730-hashplain")
@@ -85,8 +87,8 @@ func helpFunc() {
 		"sha2-512-256\n" +
 		"sha3-224 \t 17300\n" +
 		"sha3-256 \t 17400\n" +
-		"sha3-384 \t 17400\n" +
-		"sha3-512 \t 17400\n"
+		"sha3-384 \t 17500\n" +
+		"sha3-512 \t 17600\n"
 	fmt.Fprintln(os.Stderr, str)
 	os.Exit(0)
 }
